2173bc90262eb6951e3a2924bbc2894f01b000bc
hello-liang-shan <hello.liang.shan@gmail.com>
2025-11-06 13:00:51 +0800

    wip

--- src/encode.rs
+++ src/encode.rs
@@ -479,9 +479,10 @@ impl Settings {
         // Try to use the libx264 decoder. If it is not available, then use use whatever default
         // h264 decoder we have.
         Some(
-            ffmpeg::encoder::find_by_name("h264_videotoolbox")
+            ffmpeg::encoder::find_by_name("h264_nvenc")
+                .or_else(|| ffmpeg::encoder::find_by_name("hevc_nvenc"))
+                .or_else(|| ffmpeg::encoder::find_by_name("h264_videotoolbox"))
                 .or_else(|| ffmpeg::encoder::find_by_name("libx264"))
-                // .or_else()
                 .unwrap_or(ffmpeg::encoder::find(AvCodecId::H264)?),
         )
     }
